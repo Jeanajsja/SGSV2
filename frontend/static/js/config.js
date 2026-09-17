24c4c5244904d5afbbc5807c463b5aad14ac92be
Vulkan <dairojose156@gmail.com>
2026-09-17 18:04:51 +0000

    [New] Contiene la URL base y configuraciones globales

---
+++ frontend/static/js/config.js
@@ -0,0 +1,13 @@
+export const API_URL = window.location.origin + "/api";
+
+export const ROLES = {
+    ADMINISTRADOR: 1,
+    OPERATIVO: 2,
+    DOCENTE: 3
+};
+
+export const ROLES_MAP = {
+    [ROLES.ADMINISTRADOR]: 'ADMINISTRADOR',
+    [ROLES.OPERATIVO]: 'OPERATIVO',
+    [ROLES.DOCENTE]: 'DOCENTE'
+};
